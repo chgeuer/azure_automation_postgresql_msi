86a9b0919c88a76b642cb57da694d0bd98d8d64a
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-09-13 09:05:45 +0200

    a

--- serverscripts/InstallAzureAutomationPackages.ps1
+++ serverscripts/InstallAzureAutomationPackages.ps1
@@ -14,15 +14,7 @@ Install-Module -Name AzureRM.Resources -Force
 Install-Script -Name New-OnPremiseHybridWorker -Force
 
 $repo = "chgeuer/external_azureautomation_runbooks"
-$revision = "180a18dd3982c8a1e511f28d1181a8fe208feadd"
+$revision = "d83c4994f2c93232720a987609fd7d2a712de05a"
 (New-Object System.Net.WebClient).DownloadFile(
     "https://raw.githubusercontent.com/$repo/$revision/Utility/ARM/New-OnPremiseHybridWorker.ps1",
     "C:\Program Files\WindowsPowerShell\Scripts\New-OnPremiseHybridWorker.ps1");
-
-$odbcDriverArchive = "psqlodbc_11_01_0000-x64"
-$odbcURL = "https://ftp.postgresql.org/pub/odbc/versions/msi/$($odbcDriverArchive).zip"
-(New-Object System.Net.WebClient).DownloadFile($odbcURL, "$(pwd)\$($odbcDriverArchive)-msi.zip");
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$(pwd)\$($odbcDriverArchive)-msi.zip", "$(pwd)\$odbcDriverArchive")
-& msiexec.exe /quiet /qn /i "$(pwd)\$odbcDriverArchive\psqlodbc_x64.msi"
-
--- serverscripts/JoinAzureAutomationHybridWorkerGroup.ps1
+++ serverscripts/JoinAzureAutomationHybridWorkerGroup.ps1
@@ -11,27 +11,33 @@ foreach ($tag in $rawMetadata.compute.tags.split(";")) {
     $tags[$key] = $value
 }
 
-Write-Output ( "Joining VM in subscription " + $rawMetadata.compute.subscriptionId `
-    + " to automation account " + $rawMetadata.compute.resourceGroupName + "/" + `
-    $tags.automationaccountname + " (hybrid worker " + $tags.hybridgroupname + ")");
-
+Write-Output ( "Joining VM in subscription """ + $rawMetadata.compute.subscriptionId + """")
+Write-Output ( "Automation account") 
+Write-Output ( "   resource group ........ """ + $rawMetadata.compute.resourceGroupName )
+Write-Output ( "   account name .......... """ + $tags.automationaccountname + """")
+Write-Output ( "   hybrid worker group ... """ + $tags.hybridgroupname + """")
+Write-Output ( "OMS Workspace ............ """ + $tags.workspacename + """");
 
 # Tags
 # automationaccountname=chgeuerautomation
 # chgeuerautomation=chgeuerautomation_hybrid_group
 # keyvaultname=chgeuerautomationkv
+# workspacename=loganalyticsworkspacechgeuer1
 
 #
 # Must use a modified "C:\Program Files\WindowsPowerShell\Scripts\New-OnPremiseHybridWorker.ps1"
 #
 # $repo = "chgeuer/external_azureautomation_runbooks"
-# $revision = "180a18dd3982c8a1e511f28d1181a8fe208feadd"
+# $revision = "d83c4994f2c93232720a987609fd7d2a712de05a"
 # (New-Object System.Net.WebClient).DownloadFile(
 #     "https://raw.githubusercontent.com/$repo/$revision/Utility/ARM/New-OnPremiseHybridWorker.ps1",
 #     "C:\Program Files\WindowsPowerShell\Scripts\New-OnPremiseHybridWorker.ps1");
 
 New-OnPremiseHybridWorker `
-    -SubscriptionID $rawMetadata.compute.subscriptionId `
-    -AAResourceGroupName $rawMetadata.compute.resourceGroupName `
+    -SubscriptionID        $rawMetadata.compute.subscriptionId `
+    -AAResourceGroupName   $rawMetadata.compute.resourceGroupName `
+    -OMSResourceGroupName  $rawMetadata.compute.resourceGroupName `
     -AutomationAccountName $tags.automationaccountname `
-    -HybridGroupName $tags.hybridgroupname
+    -HybridGroupName       $tags.hybridgroupname `
+    -WorkspaceName         $tags.workspacename `
+    -UseManagedIdentity
