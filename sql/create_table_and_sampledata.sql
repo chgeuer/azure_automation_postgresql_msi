ab5c9cf87f0d9253f0c293ea30c697d2e7abd845
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-09-13 13:32:13 +0200

    a

--- sql/create_table_and_sampledata.sql
+++ sql/create_table_and_sampledata.sql
@@ -1,7 +1,7 @@
 --
 -- Create table schema
 --
-CREATE TABLE tenants6 (
+CREATE TABLE tenants (
     tenant_id serial PRIMARY KEY,
     tenant_name VARCHAR (50) UNIQUE NOT NULL,
     subscription_id VARCHAR (50) NOT NULL
@@ -10,7 +10,7 @@ CREATE TABLE tenants6 (
 --
 -- Populate sample data
 --
-INSERT INTO tenants6 (tenant_name, subscription_id) VALUES
+INSERT INTO tenants (tenant_name, subscription_id) VALUES
 ('Trey Research', '35125272-0253-4703-a712-e00bb6ace803'),
 ('Contoso', '961c1a03-7f8d-478c-ad3e-189b7c6044e6'),
 ('Fabrikam', '0007ef33-f284-4dfa-b27c-43ec7034f2b9');
@@ -18,4 +18,4 @@ INSERT INTO tenants6 (tenant_name, subscription_id) VALUES
 --
 -- List sample data
 --
-SELECT tenant_name, subscription_id FROM tenants6;
+SELECT tenant_name, subscription_id FROM tenants;
