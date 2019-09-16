ccb03c16934367e0d15bcfc56c6435fe2e123963
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-09-16 14:47:04 +0200

    a

--- serverscripts/RegisterVM.ps1
+++ serverscripts/RegisterVM.ps1
@@ -1,2 +1,9 @@
 .\InstallPostgresqlODBCDriverSetup.ps1 -PostgreSqlOdbcVersion "psqlodbc_11_01_0000-x64"
+
+$repo = "chgeuer/external_azureautomation_runbooks"
+$revision = "d83c4994f2c93232720a987609fd7d2a712de05a"
+(New-Object System.Net.WebClient).DownloadFile(
+    "https://raw.githubusercontent.com/$repo/$revision/Utility/ARM/New-OnPremiseHybridWorker.ps1",
+    "C:\Program Files\WindowsPowerShell\Scripts\New-OnPremiseHybridWorker.ps1");
+
 .\JoinAzureAutomationHybridWorkerGroup.ps1
